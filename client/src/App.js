27cdd682ba9da09a65b1401f38c88db2b3baa4be
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-03 20:46:44 -0400

    removed headers

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,10 @@ import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Productivity Dashboard</h1>
-      <div className="container">
-        <Homepage />
-      </div>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Homepage />
+  </div>
+);
 
 export default App;
