fe47e3b7586b832e464384554f11cf6b4299ea83
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-03 20:47:10 -0400

    added moment and style to clock

--- client/src/components/clock/clock.component.jsx
+++ client/src/components/clock/clock.component.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 
 import './clock.styles.scss';
 
 const Clock = () => {
-  const [currentTime, setCurrentTime] = useState(new Date())
+  const [currentTime, setCurrentTime] = useState(moment().format('h:mm:ss a'))
+  const [currentDate, setCurrentDate] = useState(moment().format('dddd, MMMM Do'))
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentTime(new Date());
+      setCurrentTime(moment().format('h:mm:ss a'));
+      setCurrentDate(moment().format('dddd, MMMM Do'));
     }, 1000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="clock">
-      {`${currentTime}`}
+      <div className="current-time"><h1>{currentTime}</h1></div>
+      <div><h5>{`${currentDate}`}</h5></div>
     </div>
   )
 };
