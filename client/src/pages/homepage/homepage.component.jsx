5a9640d1b09784ce9a97f7ca377d8c68a3a37f3a
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-03 20:48:53 -0400

    added css grid

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -3,15 +3,15 @@ import './homepage.styles.scss';
 
 import Clock from '../../components/clock/clock.component';
 import Stocks from '../../components/stocks/stocks.component';
-import Weather from '../../components/weather/weather.component';
+import WeatherBrief from '../../components/weather-brief/weather-brief.component';
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      <Clock />
-      <Stocks />
-      <Weather />
-    </div>
+    <section className="basic-grid">
+      <div className="card"><Clock /></div>
+      <div className="card"><WeatherBrief/></div>
+      <div className="card"><Stocks/></div>
+    </section>
   )
 };
 
