057a5d2101e84854494f007efe55a29b2f3d7788
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-04 13:50:36 -0400

    added build folder

---
+++ client/build/precache-manifest.12c9f8bd126b18fdc7d836d2eb991575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdafa5aa6f004ca02703e74714f1d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6a8de415fc03ee4c3c3",
+    "url": "/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "60dac34a14a4d6136d9b",
+    "url": "/static/css/main.7e7bbb88.chunk.css"
+  },
+  {
+    "revision": "f6a8de415fc03ee4c3c3",
+    "url": "/static/js/2.98bccd58.chunk.js"
+  },
+  {
+    "revision": "46b836aaac54df30ef4c4c7aa1c56690",
+    "url": "/static/js/2.98bccd58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60dac34a14a4d6136d9b",
+    "url": "/static/js/main.24720150.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
