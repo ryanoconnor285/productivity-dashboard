f39ea6b4a905a5c5ecc6936a632fdbacdf64b5b5
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-03 20:47:56 -0400

    initial commit

---
+++ client/src/components/calendar-monthly/calendar-monthly.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import moment from 'moment';
+
+const CalendarMonthly = () => {
+  return (
+    <div className="calendar-monthly">
+
+    </div>
+  )
+}
+
+export default CalendarMonthly;
