5f6b057f70da9c20ccf640c055193ecde899473a
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-05 00:14:29 -0400

    added a background image

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './homepage.styles.scss';
 
+import City from '../../assets/background/city-sunshards.jpg';
+import Milkyway from '../../assets/background/milkyway-night.jpg';
+
 import Clock from '../../components/clock/clock.component';
 import Stocks from '../../components/stocks/stocks.component';
 import WeatherBrief from '../../components/weather-brief/weather-brief.component';
@@ -8,12 +11,15 @@ import CalendarMonthly from '../../components/calendar-monthly/calendar-monthly.
 
 const HomePage = () => {
   return (
-    <section className="basic-grid">
-      <div className="card"><Clock /></div>
-      <div className="card"><WeatherBrief/></div>
-      <div className="card"><Stocks/></div>
-      <div className="card"><CalendarMonthly/></div>
-    </section>
+    <div>
+      <img src={Milkyway} id="background-image"/>
+      <section className="basic-grid">
+        <div className="card"><Clock /></div>
+        <div className="card"><WeatherBrief /></div>
+        <div className="card"><Stocks /></div>
+        <div className="card"><CalendarMonthly /></div>
+      </section>
+    </div>
   )
 };
 
