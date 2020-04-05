ebd2ac1fff1f5a8bcab224596639b747c6354307
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-05 12:39:37 -0400

    added hilo, sunrise, sunset, dynamic WeatherIcons

--- client/src/components/weather-brief/weather-brief.component.jsx
+++ client/src/components/weather-brief/weather-brief.component.jsx
@@ -4,12 +4,17 @@ import axios from 'axios';
 
 import './weather-brief.styles.scss';
 import WeatherHourly from '../weather-hourly/weather-hourly.component';
+import WeatherIcon from '../weather-icon/weather-icon.component';
 
 const WeatherBrief = () => {
   const [weatherBrief, setWeatherBrief] = useState({
     temperature: '',
     description: '',
     feels: '',
+    hilo: '',
+    sunrise: '',
+    sunset: '',
+    wind: ''
   });
 
   const getWeather = async () => {
@@ -20,10 +25,16 @@ const WeatherBrief = () => {
         const temperature = $('.today_nowcard-section').find('.today_nowcard-temp').text()
         const description = $('.today_nowcard-section').find('.today_nowcard-phrase').text()
         const feels = $('.today_nowcard-section').find('.today_nowcard-feels').text()
+        const hilo = `H ${$('.daypart-0 .today-daypart-temp').find('span').text()} / L ${$('.daypart-1 .today-daypart-temp').find('span').text()}`
+        const sunrise = $('#dp0-details-sunrise').text()
+        const sunset = $('#dp0-details-sunset').text()
         setWeatherBrief({
           temperature: temperature,
           description: description,
-          feels: feels
+          feels: feels,
+          hilo: hilo,
+          sunrise: sunrise,
+          sunset: sunset,
         })
         console.log(weatherBrief);
       }
@@ -36,15 +47,21 @@ const WeatherBrief = () => {
     getWeather();
     const interval = setInterval(() => {
       getWeather();
-    }, 5 * 60 * 1000);
+    }, 2 * 60 * 1000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div>
-      <div className="weather-brief">
-        <h1>{weatherBrief.temperature} {weatherBrief.description}</h1>
-        <div><h4>{weatherBrief.feels}</h4></div>
+    <div className="weather-brief">
+      <div className="weather-headline">
+        <div>{weatherBrief.description !== '' ? <WeatherIcon description={weatherBrief.description}/> : null}</div>
+        <div><h1 className="current-weather">{weatherBrief.temperature} {weatherBrief.description}</h1><h4>{weatherBrief.feels}</h4></div>
+        <div>
+          <h4>{weatherBrief.hilo}</h4>
+          <h4>Sunrise {weatherBrief.sunrise}</h4>
+          <h4>Sunset {weatherBrief.sunset}</h4>
+          <h4>{weatherBrief.wind}</h4>
+        </div>
       </div>
       <WeatherHourly />
     </div>
